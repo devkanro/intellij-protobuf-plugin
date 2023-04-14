4aab096c3042eb79df2ecea648d6b22185e12c7b
higan <higan@live.cn>
2023-04-14 15:53:21 +0800

    🚀 chore(gradle.properties): update pluginVersion to 1.7.11-EAP 🐛 fix(ProtobufFileType.kt): add workaround for issue #164 The pluginVersion is updated to 1.7.11-EAP, which is the latest version of the plugin. A workaround is added to the ProtobufFileType.kt file to fix issue #164. The workaround initializes the ProtobufStubTypes to avoid a null pointer exception.

--- src/main/kotlin/io/kanro/idea/plugin/protobuf/lang/ProtobufFileType.kt
+++ src/main/kotlin/io/kanro/idea/plugin/protobuf/lang/ProtobufFileType.kt
@@ -2,11 +2,17 @@ package io.kanro.idea.plugin.protobuf.lang
 
 import com.intellij.openapi.fileTypes.LanguageFileType
 import io.kanro.idea.plugin.protobuf.ProtobufIcons
+import io.kanro.idea.plugin.protobuf.lang.psi.stub.type.ProtobufStubTypes
 import javax.swing.Icon
 
 class ProtobufFileType : LanguageFileType(ProtobufLanguage) {
     companion object {
         val INSTANCE = ProtobufFileType()
+
+        init {
+            // Workaround for issue #164
+            ProtobufStubTypes
+        }
     }
 
     override fun getName(): String {
