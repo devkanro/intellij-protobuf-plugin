aa52e5df5fa14e83c35abb41ce6f9f71296da02f
fan-tom <lokomot476@gmail.com>
2023-09-10 17:56:50 +0400

    Add stubs to index

--- src/main/kotlin/io/kanro/idea/plugin/protobuf/java/JavaIndexProvider.kt
+++ src/main/kotlin/io/kanro/idea/plugin/protobuf/java/JavaIndexProvider.kt
@@ -10,17 +10,43 @@ class JavaIndexProvider : ProtobufIndexProvider {
     override fun buildIndex(stub: ProtobufStub<*>, sink: IndexSink) {
         when (stub) {
             is ProtobufServiceStub -> {
+                // impls
                 sink.occurrence(JavaNameIndex.key, stub.fullImplBaseName().toString())
                 sink.occurrence(JavaNameIndex.key, stub.fullCoroutineImplBaseName().toString())
+                // stubs
+                sink.occurrence(JavaNameIndex.key, stub.fullStubName().toString())
+                sink.occurrence(JavaNameIndex.key, stub.fullBlockingStubName().toString())
+                sink.occurrence(JavaNameIndex.key, stub.fullFutureStubName().toString())
+                sink.occurrence(JavaNameIndex.key, stub.fullCoroutineStubName().toString())
             }
             is ProtobufRpcStub -> {
+                val methodName = stub.methodName()
+
+                // impls
                 sink.occurrence(
                     JavaNameIndex.key,
-                    stub.owner()?.fullImplBaseName()?.append(stub.methodName()).toString()
+                    stub.owner()?.fullImplBaseName()?.append(methodName).toString()
                 )
                 sink.occurrence(
                     JavaNameIndex.key,
-                    stub.owner()?.fullCoroutineImplBaseName()?.append(stub.methodName()).toString()
+                    stub.owner()?.fullCoroutineImplBaseName()?.append(methodName).toString()
+                )
+                // stubs
+                sink.occurrence(
+                    JavaNameIndex.key,
+                    stub.owner()?.fullStubName()?.append(methodName).toString()
+                )
+                sink.occurrence(
+                    JavaNameIndex.key,
+                    stub.owner()?.fullBlockingStubName()?.append(methodName).toString()
+                )
+                sink.occurrence(
+                    JavaNameIndex.key,
+                    stub.owner()?.fullFutureStubName()?.append(methodName).toString()
+                )
+                sink.occurrence(
+                    JavaNameIndex.key,
+                    stub.owner()?.fullCoroutineStubName()?.append(methodName).toString()
                 )
             }
         }
